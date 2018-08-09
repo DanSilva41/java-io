4f81764b3bf70c998080d4d4f10d2942fbc91823
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-08-09 19:23:10 -0300

    Percorrendo dados dentro de cada linha com Scanner

--- src/br/com/danilo/alura/java/io/teste/TesteLeitura02.java
+++ src/br/com/danilo/alura/java/io/teste/TesteLeitura02.java
@@ -16,6 +16,21 @@ public class TesteLeitura02 {
         while (scanner.hasNextLine()) {
             linha = scanner.nextLine();
             System.out.println(linha);
+
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useLocale(Locale.US); // Definindo Locale para evitar diferenca de regiao
+            linhaScanner.useDelimiter(",");
+            String valor1 = linhaScanner.next();
+            int valor2 = linhaScanner.nextInt();
+            int valor3 = linhaScanner.nextInt();
+            String valor4 = linhaScanner.next();
+            double valor5 = linhaScanner.nextDouble();
+
+            System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+//            String[] valores = linha.split(",");
+//               System.out.println(valores);
+//            System.out.println(Arrays.toString(valores));
+//            System.out.println(valores[3]); // Titular
         }
 
         scanner.close();
